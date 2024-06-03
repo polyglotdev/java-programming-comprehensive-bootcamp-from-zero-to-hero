7bee8b47637248796c5bcd8464f05f604ca53e43
Dom Hallan <dom@domhallan.com>
2024-06-03 14:31:02 -0400

    Added new modules and updated .gitignore
    Two new modules, 'hello' and 'section2', have been added to the project. Each module includes a pom.xml file for Maven configuration and a Main.java file with basic code. The .gitignore files in both the root directory and the new modules have been updated to ignore unnecessary files from various IDEs, build tools, and operating systems.

---
+++ hello/src/main/java/com/domhallan/Main.java
@@ -0,0 +1,9 @@
+package com.domhallan;
+
+public class Main {
+  public static void main(String[] args) {
+    var name = "Elijah";
+    var greeting = String.format("Hello, %s!", name);
+    System.out.println(greeting);
+  }
+}
