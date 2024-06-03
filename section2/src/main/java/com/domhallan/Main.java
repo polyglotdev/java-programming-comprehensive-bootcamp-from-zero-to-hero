9e837a49cf50944831a3d80c68583e6b87670936
Dom Hallan <dom@domhallan.com>
2024-06-03 14:41:40 -0400

    Added conditional logic and string formatting
    Introduced a boolean variable with an if-else statement to print different outputs based on its value. Also added a new string variable and used String.format for personalized greeting message output.

--- section2/src/main/java/com/domhallan/Main.java
+++ section2/src/main/java/com/domhallan/Main.java
@@ -2,6 +2,15 @@ package com.domhallan;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Section 2");
+    boolean b = false;
+    if (b) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+
+    String name = "Elijah";
+    var greeting = String.format("%s is my name!", name);
+    System.out.println(greeting);
   }
 }
