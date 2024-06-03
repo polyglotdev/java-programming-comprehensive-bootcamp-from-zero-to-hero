acce5150e07289cabc49811daeffffbe24d2934e
Dom Hallan <dom@domhallan.com>
2024-06-03 15:23:44 -0400

    Added new Person class with attributes and methods
    A new class, Person, has been introduced. This class includes private attributes such as name, age, gender, nationality, and career. Corresponding getter and setter methods for these attributes have also been added to allow access and modification.

---
+++ Section4/src/main/java/com/domhallan/Person.java
@@ -0,0 +1,57 @@
+package com.domhallan;
+
+public class Person {
+  private String name;
+  private int age;
+  private String gender;
+  private String nationality;
+  private String career;
+
+  public Person(String name, int age, String gender, String nationality, String career) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.nationality = nationality;
+    this.career = career;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getNationality() {
+    return nationality;
+  }
+
+  public void setNationality(String nationality) {
+    this.nationality = nationality;
+  }
+
+  public String getCareer() {
+    return career;
+  }
+
+  public void setCareer(String career) {
+    this.career = career;
+  }
+}
