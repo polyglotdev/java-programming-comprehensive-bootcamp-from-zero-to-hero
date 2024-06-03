8438baeee15845e69d4e1fef18c56e7e61fcb21e
Dom Hallan <dom@domhallan.com>
2024-06-03 14:56:10 -0400

    Refactored greeting logic into HelloWorld class
    The main method's greeting logic has been refactored. Instead of directly creating a greeting string, we now instantiate a HelloWorld object and use its methods to set the name and generate the greeting. This makes the code more modular and easier to maintain.

--- HelloWorld/src/main/java/com/domhallan/Main.java
+++ HelloWorld/src/main/java/com/domhallan/Main.java
@@ -2,8 +2,9 @@ package com.domhallan;
 
 public class Main {
   public static void main(String[] args) {
-    var name = "Elijah";
-    String greeting = String.format("Hello, %s!", name);
-    System.out.println(greeting);
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.setName("Elijah");
+    var greet = helloWorld.sayHello();
+    System.out.println(greet);
   }
 }
