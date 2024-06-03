8c3d820cdafd03988f3980abd9c1ceef9c679207
Dom Hallan <dom@domhallan.com>
2024-06-03 14:52:25 -0400

    Added HelloWorld project with Maven support
    This commit introduces a new HelloWorld project. The project is set up with Maven for dependency management and build automation. A .gitignore file has been added to exclude unnecessary files from version control, covering various IDEs and operating systems.
    
    The main application logic resides in two Java classes: HelloWorld and Main. The HelloWorld class contains a private String variable 'name' along with its getter and setter methods. The Main class includes the main method where a greeting message is created and printed to the console using the name "Elijah".

---
+++ HelloWorld/src/main/java/com/domhallan/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.domhallan;
+
+public class HelloWorld {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ HelloWorld/src/main/java/com/domhallan/Main.java
@@ -0,0 +1,9 @@
+package com.domhallan;
+
+public class Main {
+  public static void main(String[] args) {
+    var name = "Elijah";
+    String greeting = String.format("Hello, %s!", name);
+    System.out.println(greeting);
+  }
+}
