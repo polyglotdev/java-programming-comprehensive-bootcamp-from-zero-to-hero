9da058b9eff1bdb886beeabbe16012c154a80eb4
Dom Hallan <dom@domhallan.com>
2024-06-06 16:13:01 -0400

    Enhanced array functionality and added NBA teams list
    The code now includes an additional import for java.util.List. The main function in ArraysBasics class has been expanded to include a new List of NBA teams, which is then printed out. Additionally, the length of the mobileNumbers array is now being printed out as well.

--- Section12/src/main/java/com/domhallan/main/ArraysBasics.java
+++ Section12/src/main/java/com/domhallan/main/ArraysBasics.java
@@ -1,6 +1,7 @@
 package com.domhallan.main;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ArraysBasics {
   public static void main(String[] args) {
@@ -9,6 +10,20 @@ public class ArraysBasics {
 
     for (String mobileNumber : mobileNumbers) {
       System.out.println("Mobile number: " + mobileNumber);
+    }
+
+    System.out.println("Mobile numbers length: " + mobileNumbers.length);
+
+    List<String> nbaTeams = List.of("Los Angeles Lakers",
+        "Sacramento Kings", "Golden State Warriors", "Utah Jazz",
+        "Los Angeles Clippers", "Chicago Bulls", "Miami Heat",
+        "Dallas Mavericks", "New York Knicks", "Philadelphia 76ers",
+        "Boston Celtics", "Orlando Magic", "Indiana Pacers",
+        "Milwaukee Bucks", "Toronto Raptors",
+        "Memphis Grizzles");
+
+    for (String nbaTeam : nbaTeams) {
+      System.out.println(nbaTeam);
     }
   }
 }
