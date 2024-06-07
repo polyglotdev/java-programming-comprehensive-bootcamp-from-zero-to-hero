9826f56e981a6f37f81bbb870651da4fb80c86f3
Dom Hallan <dom@domhallan.com>
2024-06-07 12:58:30 -0400

    Added .gitignore, pom.xml and new Java classes
    Created a .gitignore file to exclude unnecessary files from the repository. Added a pom.xml file for Maven project configuration. Introduced two new Java classes: ArrayListDemo and Main. The ArrayListDemo class demonstrates usage of ArrayLists with strings and integers, while the Main class contains a simple "Hello world!" print statement.

---
+++ Section17/src/main/java/com/domhallan/list/ArrayListDemo.java
@@ -0,0 +1,48 @@
+package com.domhallan.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("Hello");
+    list.add("world");
+    System.out.println(list);
+
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(100);
+    numbers.add(200);
+    numbers.add(300);
+    var total = 0;
+    for (Integer number : numbers) {
+      total += number;
+    }
+    System.out.println(total);
+
+    List<String> countryNames = getStrings();
+
+    for (String countryName : countryNames) {
+      System.out.println(countryName);
+    }
+  }
+
+  private static List<String> getStrings() {
+    List<String> countryNames = new ArrayList<>();
+    countryNames.add("United States of America");
+    countryNames.add("Canada");
+    countryNames.add("Mexico");
+    countryNames.add("Brazil");
+    countryNames.add("Argentina");
+    countryNames.add("Chile");
+    countryNames.add("Peru");
+    countryNames.add("Colombia");
+    countryNames.add("Venezuela");
+    countryNames.add("Uruguay");
+    countryNames.add("Paraguay");
+    countryNames.add("Ecuador");
+    countryNames.add("Bolivia");
+    countryNames.add("Guyana");
+    return countryNames;
+  }
+}
