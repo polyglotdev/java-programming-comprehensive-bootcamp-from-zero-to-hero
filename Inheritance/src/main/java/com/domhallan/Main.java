594665731122dcbc37d4b3d1c7eaaadeda7a0937
Dom Hallan <dom@domhallan.com>
2024-06-07 12:33:30 -0400

    Added Iron Man to main program
    Iron Man has been introduced into the main program. The new code includes creating an instance of Iron Man, invoking his actions such as using power, eating, walking and sleeping. Also, his age is set and printed out. Additionally, a minor change was made to Captain America's age print statement for better readability.

--- Inheritance/src/main/java/com/domhallan/Main.java
+++ Inheritance/src/main/java/com/domhallan/Main.java
@@ -1,6 +1,7 @@
 package com.domhallan;
 
 import com.domhallan.marvel.heros.CaptainAmerica;
+import com.domhallan.marvel.heros.IronMan;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,6 +13,15 @@ public class Main {
     captainAmerica.sleep();
     captainAmerica.age = 100;
 
-    System.out.println("Captain America's age is " + captainAmerica.age);
+    System.out.println("Captain America's age is " + captainAmerica.age + " years old.");
+
+    IronMan ironMan = new IronMan();
+    ironMan.usePower();
+    ironMan.eat("pizza");
+    ironMan.walk();
+    ironMan.sleep();
+    ironMan.age = 50;
+
+    System.out.println("Iron Man is " + ironMan.age + " years old.");
   }
 }
